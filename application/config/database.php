e9a03e86f3b75a04fba427e5d9ee0f4746b6f094
maransubs <maransubs@gmail.com>
2025-11-19 15:52:31 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'test.cbmouss2so7f.ap-south-1.rds.amazonaws.com',
+	'hostname' => 'cmtsdb.cnmycsqywjl9.ap-south-1.rds.amazonaws.com',
 	'username' => 'admin',
 	'password' => 'admin123',
 	'database' => 'college_mgmt',
